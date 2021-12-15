0e96a4716c8379da37a1b46b8a88ed25c7969b0a
Kyle King <KyleKing@users.noreply.github.com>
2021-12-15 18:46:16 -0500

    refactor: move z to zprofile and cleanup zshrc

--- dot_config/my_config/private__python.sh
+++ dot_config/my_config/private__python.sh
@@ -1,5 +1,11 @@
 # Python, pipx, and pyenv Configuration
 
+# Configure pyenv
+# https://github.com/pyenv/pyenv/issues/849#issuecomment-285229619
+export PYENV_ROOT="$HOME/.pyenv"
+export PATH="$PYENV_ROOT/bin:$PATH"
+eval "$(pyenv init --path)"
+
 # > pipx completions require bashcompinit
 # autoload -U bashcompinit
 # bashcompinit
--- dot_config/my_config/private_cli_tools.sh
+++ dot_config/my_config/private_cli_tools.sh
@@ -8,27 +8,3 @@ alias ls='exa --icons'
 alias lsa='ls --all'
 alias lsl='ls --long --header --created --git'
 alias lst='lsl -TL 2'
-
-# # https://github.com/junegunn/fzf
-# # To install useful key bindings and fuzzy completion:
-# # Run once!
-# # $(brew --prefix)/opt/fzf/install
-# # To uninstall!
-# # $(brew --prefix)/opt/fzf/uninstall
-# [ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
-# # cat $(fzf)
-# https://github.com/ajeetdsouza/zoxide
-# After fzf
-eval "$(zoxide init zsh)"
-# zoxide query  --list --score
-# z foo              # cd into highest ranked directory matching foo
-# z foo bar          # cd into highest ranked directory matching foo and bar
-#
-# z ~/foo            # z also works like a regular cd command
-# z foo/             # cd into relative path
-# z ..               # cd one level up
-# z -                # cd into previous directory
-#
-# zi foo             # cd with interactive selection (using fzf)
-#
-# z foo<SPACE><TAB>  # show interactive completions (zoxide v0.7.10+, bash/fish/zsh only)
