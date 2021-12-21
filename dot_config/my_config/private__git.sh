8406ccda02a05f9abb18abe87f35c2ae7b4a43de
Kyle King <KyleKing@users.noreply.github.com>
2021-12-21 06:49:07 -0500

    fix: re-add clone-pr alias

--- dot_config/my_config/private__git.sh
+++ dot_config/my_config/private__git.sh
@@ -9,17 +9,15 @@ lzgcd() {
     lazygit "$@"
 
     if [ -f $LAZYGIT_NEW_DIR_FILE ]; then
-            cd "$(cat $LAZYGIT_NEW_DIR_FILE)"
+            cd "$(cat $LAZYGIT_NEW_DIR_FILE)" || exit
             rm -f $LAZYGIT_NEW_DIR_FILE > /dev/null
     fi
 }
 alias lzg='lazygit'
 
 export PR_CHECKOUT_DIR=~/developer/checkouts
-if [[ -d $PR_CHECKOUT_DIR ]] {
-    alias clone-pr() {
-        mkcd $PR_CHECKOUT_DIR && gh repo clone $1 $1/pr$2 && cd $1/pr$2 && gh pr checkout $2 --force
-    }
+clone-pr() {
+    mkcd $PR_CHECKOUT_DIR && gh repo clone $1 $1/pr$2 && cd $1/pr$2 && gh pr checkout $2 --force
 }
 # clone-pr timothycrosley/pdocs 25
 # clone-pr dash_charts 25
