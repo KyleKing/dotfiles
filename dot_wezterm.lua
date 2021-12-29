e08c076f6fb85776a15331427dc8394d076ec6f8
Kyle King <KyleKing@users.noreply.github.com>
2021-12-29 07:51:46 -0500

    feat: add Tokyo Night Storm variant!

--- dot_wezterm.lua
+++ dot_wezterm.lua
@@ -43,7 +43,40 @@ return {
 
   -- Colors: https://wezfurlong.org/wezterm/config/appearance.html
   -- Note that "color_scheme" overrides "colors"
-  color_scheme = "Firewatch",
+  -- color_scheme = "Firewatch",
+  --
+  -- Based on "Tokyo Night Storm variant": https://github.com/enkia/tokyo-night-vscode-theme#other-portings
+  -- For Wez: https://github.com/wez/wezterm/blob/main/assets/colors/3024%20Day.toml
+  colors = {
+      -- The default text color
+      foreground = "#a9b1d6",
+      -- The default background color
+      background = "#24283b",
+
+      -- TODO: Overrides the cell background color when the current cell is occupied by the
+      -- cursor and the cursor style is set to Block
+      cursor_bg = "#52ad70",
+      -- TODO: Overrides the text color when the current cell is occupied by the cursor
+      cursor_fg = "black",
+      -- TODO: Specifies the border color of the cursor when the cursor style is set to Block,
+      -- or the color of the vertical or horizontal bar when the cursor style is set to
+      -- Bar or Underline.
+      cursor_border = "#52ad70",
+
+      -- TODO: the foreground color of selected text
+      selection_fg = "black",
+      -- TODO: the background color of selected text
+      selection_bg = "#fffacd",
+
+      -- TODO: The color of the scrollbar "thumb"; the portion that represents the current viewport
+      scrollbar_thumb = "#222222",
+
+      -- TODO: The color of the split lines between panes
+      split = "#444444",
+
+      ansi = {"#32344a", "#f7768e", "#9ece6a", "#e0af68", "#7aa2f7", "#ad8ee6", "#449dab", "#9699a8"},
+      brights = {"#444b6a", "#ff7a93", "#b9f27c", "#ff9e64", "#7da6ff", "#bb9af7", "#0db9d7", "#acb0d0"},
+  },
 
   -- Stylize the Window
   window_decorations = "RESIZE",
