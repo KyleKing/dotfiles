cdda403888e24585f2e05a4ec0af50290373a3aa
Kyle King <KyleKing@users.noreply.github.com>
2022-01-06 21:17:49 -0500

    feat: more git snippets

--- dot_config/my_config/private__git.sh
+++ dot_config/my_config/private__git.sh
@@ -2,6 +2,15 @@
 #      ^----- get shellcheck hints based on bash
 # https://github.com/koalaman/shellcheck/issues/809#issuecomment-631194320
 
+# Set the upstream branch
+# From: https://github.com/ornicar/dotfiles/blob/7f0940aa42b7c79771ad1fe31be21cd49827f161/zsh/git-functions.zsh#L35-L40
+git-set-upstream() {
+    # Use: "git-set-upstream"
+    [ -z $1 ] && 1=origin
+    branch=$(git-current-branch)
+    git push $1 $branch --set-upstream
+}
+
 # Source: https://github.com/jesseduffield/lazygit#changing-directory-on-exit
 lzgcd() {
     export LAZYGIT_NEW_DIR_FILE=~/.lazygit/newdir
@@ -21,7 +30,7 @@ clone-pr() {
     # Use with: "clone-pr timothycrosley/pdocs 25" or "clone-pr dash_charts 25"
     mkcd $PR_CHECKOUT_DIR
     gh repo clone $1 $1-pr$2
-    cd $1-pr$2
+    cd $1-pr$2 || exit
     gh pr checkout $2 --force
     subl . --new-window
     z .
