21e49188519fadff1db5f3f56c3f8a3a5e3fe5b1
Kyle King <KyleKing@users.noreply.github.com>
2022-01-06 20:32:50 -0500

    feat: try to add image diff to lazygit

---
+++ dot_local/bin/executable_imgdiff.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# Source: https://til.hashrocket.com/posts/n44hnf0z99-show-better-image-diff-on-git
+
+if [[ -f "$1" ]] && [[ -f "$2" ]]; then
+  compare "$2" "$1" png:- | montage -geometry +4+4 "$2" - "$1" png:- | wezterm imgcat
+else
+  if [[ -f "$1" ]]; then
+    echo "+ Image Added"
+    wezterm imgcat "$1"
+  else
+    echo "- Image Removed"
+    wezterm imgcat "$2"
+  fi
+fi
+
+exit 0
