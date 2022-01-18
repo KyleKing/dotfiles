3ca6b4c0acd1fc90d4f0c6fbe3c471dd0c65a1d7
Kyle King <KyleKing@users.noreply.github.com>
2022-01-18 10:30:01 -0500

    refactor: simplify Exact Quick Find shortcuts to only word and caps

--- dot_config/my_config/private_homebrew.sh
+++ dot_config/my_config/private_homebrew.sh
@@ -13,3 +13,5 @@ fi
 # Fix Brew Doctor warning about" config scripts exist outside your system or Homebrew directories"
 # From: https://github.com/pyenv/pyenv
 alias brew='env PATH="${PATH//$(pyenv root)\/shims:/}" brew'
+
+# FIXME: install fd once we can upgrade our OS
