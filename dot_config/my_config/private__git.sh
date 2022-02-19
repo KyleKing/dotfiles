5d311fd72496f901056c4924d3d0c6ec7115d427
Kyle King <KyleKing@users.noreply.github.com>
2022-02-19 07:21:31 -0500

    refactor: create separate Sublime alias file

--- dot_config/my_config/private__git.sh
+++ dot_config/my_config/private__git.sh
@@ -42,28 +42,6 @@ clone-pr() {
     cp ~/.config/my_config/pyrightconfig.json pyrightconfig.json
 }
 
-# FIXME: Create new project file per project with path to the .venv Python
-# Then open with subl using the "--project" arg
-# // https://www.sublimetext.com/docs/projects.html
-# // https://www.sublimelinter.com/en/stable/settings.html#project-settings
-# // Check "show_hover_line_report" and "show_hover_region_report"
-# {
-#     "folders": [{"path": "/Users/kyleking/Developer/recipes"}],,
-#     "settings": {
-#         "SublimeLinter.linters.flake8.args": [
-#             "--max-line-length=88",
-#             "--ignore=B902,D100,D103,D205,D209,D400,D401,D403,E203,E501,E722,W503",
-#             "--select=A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z"
-#         ],
-#         "SublimeLinter.linters.flake8.python": "$project_path/.env/bin/python",
-#         "SublimeLinter.linters.pylint.args": [
-#             "--disable=E0012,E0401,W0613,C0115,C0116,W1203",
-#             "--rcfile=./reviewer-api/.pylintrc"
-#         ],
-#         "SublimeLinter.linters.pylint.python": "$project_path/.env/bin/python"
-#     }
-# }
-
 # TODO: need script to walk the PR_CHECKOUT_DIR for git directories, check if the branch was merged, then delete the folder
 # FYI: Make sure to check for a "git stash" and error on any that have one
 # These aliases could possibly be a gh plugin? Could fork "gh poi" or "gh tidy"?
---
+++ dot_config/my_config/private__sublime.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#      ^----- get shellcheck hints based on bash
+# https://github.com/koalaman/shellcheck/issues/809#issuecomment-631194320
+
+# FIXME: Create new project file per project with path to the .venv Python
+# Then open with subl using the "--project" arg
+# // https://www.sublimetext.com/docs/projects.html
+# // https://www.sublimelinter.com/en/stable/settings.html#project-settings
+# // Check "show_hover_line_report" and "show_hover_region_report"
+# {
+#     "folders": [{"path": "/Users/kyleking/Developer/recipes"}],,
+#     "settings": {
+#         "SublimeLinter.linters.flake8.args": [
+#             "--max-line-length=88",
+#             "--ignore=B902,D100,D103,D205,D209,D400,D401,D403,E203,E501,E722,W503",
+#             "--select=A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z"
+#         ],
+#         "SublimeLinter.linters.flake8.python": "$project_path/.env/bin/python",
+#         "SublimeLinter.linters.pylint.args": [
+#             "--disable=E0012,E0401,W0613,C0115,C0116,W1203",
+#             "--rcfile=./reviewer-api/.pylintrc"
+#         ],
+#         "SublimeLinter.linters.pylint.python": "$project_path/.env/bin/python"
+#     }
+# }
