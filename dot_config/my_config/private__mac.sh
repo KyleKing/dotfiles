0630d06688138c608e1f1ab76de02f9c00346b02
Kyle King <KyleKing@users.noreply.github.com>
2022-02-20 12:39:04 -0500

    feat: add Dropover CLI shortcut!

---
+++ dot_config/my_config/private__mac.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#      ^----- get shellcheck hints based on bash
+# https://github.com/koalaman/shellcheck/issues/809#issuecomment-631194320
+# Oh My ZSH (OMZ) Configuration
+
+# General Mac Aliases
+
+# Open with Dropover!
+alias odo="open -a Dropdover"
