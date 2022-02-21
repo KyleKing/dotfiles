c13d666b4babe15825d9c6a9822582781045e685
Kyle King <KyleKing@users.noreply.github.com>
2022-02-21 07:25:11 -0500

    fix: use bo and bi to differentiate from brew link

--- dot_config/my_config/private_homebrew.sh
+++ dot_config/my_config/private_homebrew.sh
@@ -20,6 +20,6 @@ alias brew='env PATH="${PATH//$(pyenv root)\/shims:/}" brew'
 
 # A few helpful snippets not included in the homebrew zsh plugin
 alias bs="brew search"
-alias bi="brew info"
-alias bl="brew install"
+alias bo="brew info"
+alias bi="brew install"
 alias bcbd="brew cleanup && brew doctor"
