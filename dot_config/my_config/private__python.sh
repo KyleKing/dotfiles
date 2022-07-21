378808a13c65d7bdeea7bb2420e38b30b3f23c5c
Kyle King <KyleKing@users.noreply.github.com>
2022-07-21 17:14:40 -0400

    build: fully switch to asdf from pyenv

--- dot_config/my_config/private__python.sh
+++ dot_config/my_config/private__python.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #      ^----- get shellcheck hints based on bash
 # https://github.com/koalaman/shellcheck/issues/809#issuecomment-631194320
-# Python, pipx, and pyenv Configuration
+# Python and pipx Configuration
 
 # Ensure that pre-commit always uses color
 # https://github.com/pre-commit/pre-commit/blob/354b900f15e88a06ce8493e0316c288c44777017/pre_commit/color.py#L105
@@ -9,12 +9,6 @@ export PRE_COMMIT_COLOR=always
 
 # Turn On Better Exceptions for Python
 export BETTER_EXCEPTIONS=1
-
-# # Configure pyenv
-# # Following: https://github.com/pyenv/pyenv#homebrew-in-macos
-# # Path configuration is in .zshprofile
-# # Note: https://github.com/pyenv/pyenv/issues/849#issuecomment-285229619
-# eval "$(pyenv init -)"
 
 # > pipx completions require bashcompinit
 # autoload -U bashcompinit
