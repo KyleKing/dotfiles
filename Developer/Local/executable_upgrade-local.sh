36d43ff6e91d6f7bca7cfa5e9f63836909d8abe0
Kyle King <kyleking@roaminsight.com>
2023-06-23 08:47:15 -0400

    feat: sync the firefox vertical tabs repo with instructions on symlinking

--- Developer/Local/executable_upgrade-local.sh
+++ Developer/Local/executable_upgrade-local.sh
@@ -2,3 +2,17 @@
 
 original_dir=$PWD
 for pth in gotz lazygit "sh/cmd/shfmt"; do echo "Upgrading $pth" && cd "$pth" && git pull && go install . && cd "$original_dir"; done
+
+pth="firefox-vertical-tabs"
+echo "Upgrading $pth" && cd "$pth" && git pull && cd "$original_dir"
+# # Note: symlink this project into the Firefox profiles (id's might differ per system)
+# > cd /Users/kyleking/Library/Application\ Support/Firefox/Profiles
+# > lsl
+# Permissions Size User     Date Created Name
+# drwx------@    - kyleking  2 Sep  2022  4c2wgopw.dev-edition-default
+# drwx------@    - kyleking  2 Sep  2022  6zuyes7x.default-release
+# drwx------@    - kyleking  2 Sep  2022  trii2qfj.default
+# > mkcd 4c2wgopw.dev-edition-default/chrome
+# > ln -s /Users/kyleking/Developer/local/firefox-vertical-tabs/userChrome.css userChrome.css
+# > ...
+# > # Repeat
