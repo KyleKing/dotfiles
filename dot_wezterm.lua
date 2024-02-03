bcc1eaf96841cfa9220df7f569594306280f1eb7
Kyle King <KyleKing@users.noreply.github.com>
2024-02-03 03:59:06 -0500

    refactor: cleanup tab color logic

--- dot_wezterm.lua
+++ dot_wezterm.lua
@@ -27,7 +27,6 @@ local wezterm = require("wezterm")
 -- Configuration for Tab Color
 
 -- Based on: https://github.com/protiumx/.dotfiles/blob/854d4b159a0a0512dc24cbc840af467ac84085f8/stow/wezterm/.config/wezterm/wezterm.lua#L291-L319
-
 local process_icons = {
     ["bash"] = wezterm.nerdfonts.cod_terminal_bash,
     ["btm"] = wezterm.nerdfonts.mdi_chart_donut_variant,
@@ -56,129 +55,122 @@ local process_icons = {
     ["wget"] = wezterm.nerdfonts.mdi_arrow_down_box,
     ["zsh"] = wezterm.nerdfonts.dev_terminal,
 }
+
+-- Return the Tab's current working directory
 local function get_cwd(tab)
     -- Returns URL Object: https://wezfurlong.org/wezterm/config/lua/pane/get_current_working_dir.html
     return tab.active_pane.current_working_dir.file_path or ""
 end
+
+-- Remove all path components and return only the last value
+local function remove_abs_path(path) return path:gsub("(.*[/\\])(.*)", "%2") end
+
+-- Return the pretty path of the tab's current working directory
 local function get_display_cwd(tab)
     local current_dir = get_cwd(tab)
     local HOME_DIR = string.format("file://%s", os.getenv("HOME"))
-
-    return current_dir == HOME_DIR and "~/" or current_dir:gsub("(.*[/\\])(.*)", "%2")
+    return current_dir == HOME_DIR and "~/" or remove_abs_path(current_dir)
 end
-local function get_process(tab)
-    if not tab.active_pane or tab.active_pane.foreground_process_name == "" then
-        return "[?]"
-    end
 
-    local process_name = tab.active_pane.foreground_process_name:gsub("(.*[/\\])(.*)", "%2")
-    if process_name:find("kubectl") then
-        process_name = "kubectl"
-    end
+-- Return the concise name or icon of the running process for display
+local function get_process(tab)
+    if not tab.active_pane or tab.active_pane.foreground_process_name == "" then return "[?]" end
+
+    local process_name = remove_abs_path(tab.active_pane.foreground_process_name)
+    if process_name:find("kubectl") then process_name = "kubectl" end
 
     return process_icons[process_name] or string.format("[%s]", process_name)
 end
+
+-- Pretty format the tab title
 local function format_title(tab)
     local cwd = get_display_cwd(tab)
     local process = get_process(tab)
 
     local active_title = tab.active_pane.title
-    if active_title:find("- NVIM") then
-        active_title = active_title:gsub("^([^ ]+) .*", "%1")
-    end
+    if active_title:find("- NVIM") then active_title = active_title:gsub("^([^ ]+) .*", "%1") end
 
     local description = (not active_title or active_title == cwd) and "~" or active_title
     return string.format(" %s %s/ %s ", process, cwd, description)
 end
+
 -- Determine if a tab has unseen output since last visited
 local function hasUnseenOutput(tab)
     if not tab.is_active then
         for _, pane in ipairs(tab.panes) do
-            if pane.has_unseen_output then
-                return true
-            end
+            if pane.has_unseen_output then return true end
         end
     end
     return false
 end
 
--- This function returns the suggested title for a tab. It prefers the title that was set via `tab:set_title()`
---  or `wezterm cli set-tab-title` or creates a new representative title
--- Based on: https://wezfurlong.org/wezterm/config/lua/window-events/format-tab-title.html
+-- Returns manually set title (from `tab:set_title()` or `wezterm cli set-tab-title`) or creates a new one
 local function get_tab_title(tab)
     local title = tab.tab_title
     -- if the tab title is explicitly set, take that
-    if title and #title > 0 then
-        return title
-    end
+    if title and #title > 0 then return title end
     return format_title(tab)
 end
 
--- Convert arbitrary strings to a unique hex value
+-- Convert arbitrary strings to a unique hex color value
 -- Based on: https://stackoverflow.com/a/3426956/3219667
-local function hashCode(str)
+local function string_to_color(str)
+    -- Convert the string to a unique integer
     local hash = 0
     for i = 1, #str do
         hash = string.byte(str, i) + ((hash << 5) - hash)
     end
-    return hash
-end
-local function intToHex(i)
-    local c = string.format("%06X", i & 0x00FFFFFF)
+
+    -- Convert the integer to a unique color
+    local c = string.format("%06X", hash & 0x00FFFFFF)
     return "#" .. (string.rep("0", 6 - #c) .. c):upper()
 end
-local testColor = intToHex(hashCode("/Users/kyleking/Developer/ProjectA"))
-assert(testColor == "#EBD168", "Unexpected color value for test hash (" .. testColor .. ")")
 
--- Send this function a luminance value between 0.0 and 1.0, and it returns L* which is "perceptual lightness"
--- Based on: https://stackoverflow.com/a/56678483/3219667
-local function calculateLuminance(hexColor)
-    local color = hexColor:gsub("#", "") -- Remove leading '#'
+local function selectContrastingForeground(hex_color)
+    -- Based on: https://stackoverflow.com/a/56678483/3219667
+    local function calculate_luminance(color)
+        -- Extract RGB components from hex color
+        local red, green, blue
+        red = tonumber(color:sub(1, 2), 16)
+        green = tonumber(color:sub(3, 4), 16)
+        blue = tonumber(color:sub(5, 6), 16)
+        -- Calculate the luminance of the given color and compare against perceived brightness
+        return 0.2126 * red / 255 + 0.7152 * green / 255 + 0.0722 * blue / 255
+    end
 
-    -- Extract RGB components from hex color
-    local red, green, blue
-    red = tonumber(color:sub(1, 2), 16)
-    green = tonumber(color:sub(3, 4), 16)
-    blue = tonumber(color:sub(5, 6), 16)
-
-    -- Calculate the luminance of the given color and compare against perceived brightness
-    return 0.2126 * red / 255 + 0.7152 * green / 255 + 0.0722 * blue / 255
-end
--- local function YtoLstar(luminance)
---     assert(0 <= luminance and luminance <= 1, "luminance is not within [0, 1]")
---     if luminance <= (216 / 24389) then -- The CIE standard states 0.008856 but 216/24389 is the intent for 0.008856451679036
---         return luminance * (24389 / 27) -- The CIE standard states 903.3, but 24389/27 is the intent, making 903.296296296296296
---     end
---     return luminance ^ (1 / 3) * 116 - 16
--- end
-local function selectContrastingForeground(hexColor)
-    local luminance = calculateLuminance(hexColor)
+    local color = hex_color:gsub("#", "") -- Remove leading '#'
+    local luminance = calculate_luminance(color)
     if luminance > 0.5 then
         return "#000000" -- Black has higher contrast with colors perceived to be "bright"
     end
     return "#FFFFFF" -- White has higher contrast
 end
--- assert(YtoLstar(calculateLuminance("#494CED")) >= 64, "Expected lightness of around 65")
+
+-- Inline tests
+local testColor = string_to_color("/Users/kyleking/Developer/ProjectA")
+assert(testColor == "#EBD168", "Unexpected color value for test hash (" .. testColor .. ")")
 assert(selectContrastingForeground("#494CED") == "#FFFFFF", "Expected higher contrast with white")
 assert(selectContrastingForeground("#128b26") == "#FFFFFF", "Expected higher contrast with white")
 assert(selectContrastingForeground("#58f5a6") == "#000000", "Expected higher contrast with black")
 assert(selectContrastingForeground("#EBD168") == "#000000", "Expected higher contrast with black")
 
-wezterm.on("format-tab-title", function(tab, _tabs, _panes, _config, _hover, _max_width)
+-- On format tab title events, override the default handling to return a custom title
+-- Docs: https://wezfurlong.org/wezterm/config/lua/window-events/format-tab-title.html
+---@diagnostic disable-next-line: unused-local
+wezterm.on("format-tab-title", function(tab, tabs, panes, config, hover, max_width)
     local title = get_tab_title(tab)
-    local color = intToHex(hashCode(get_cwd(tab)))
+    local color = string_to_color(get_cwd(tab))
 
     if tab.is_active then
         return {
             { Attribute = { Intensity = "Bold" } },
             { Background = { Color = color } },
             { Foreground = { Color = selectContrastingForeground(color) } },
-            { Text = " " .. title .. " " },
+            { Text = title },
         }
     end
     if hasUnseenOutput(tab) then
         return {
-            { Attribute = { Intensity = "Bold" } },
             { Foreground = { Color = "#EBD168" } },
             { Text = title },
         }
