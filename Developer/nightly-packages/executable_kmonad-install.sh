4e50007d1293da1304e2dd42767498d96f06c2bb
Kyle King <KyleKing@users.noreply.github.com>
2024-03-14 05:09:25 -0400

    refactor: remove kmonad after installation issues

--- Developer/nightly-packages/executable_kmonad-install.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-# dext is installed by karabiner-elements (brew install --cask karabiner-elements)
-# stack is installed with brew (brew install haskell-stack)
-#
-# Docs: https://github.com/kmonad/kmonad/blob/master/doc/installation.md#installing-kmonad
-cd ./kmonad || exit
-stack build --flag kmonad:dext --extra-include-dirs=c_src/mac/Karabiner-DriverKit-VirtualHIDDevice/include/pqrs/karabiner/driverkit:c_src/mac/Karabiner-DriverKit-VirtualHIDDevice/src/Client/vendor/include
