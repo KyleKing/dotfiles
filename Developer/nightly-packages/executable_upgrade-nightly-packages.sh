c62c1f859384197fcbcc7a4e345117e0bf1fe3d1
Kyle King <KyleKing@users.noreply.github.com>
2024-06-18 20:42:57 -0400

    fix: use mani for nightly packages

--- Developer/nightly-packages/executable_upgrade-nightly-packages.sh
+++ Developer/nightly-packages/executable_upgrade-nightly-packages.sh
@@ -2,32 +2,10 @@
 
 # FYI: if adding any CLI tools, make sure to also update dump-version.sh
 
+mani run install --tags=go-install
+
 original_dir=$PWD
 
-for pth in cs gotz "Kube-Node-Usage" lazygit "sh/cmd/shfmt" superfile; do
-    echo "Upgrading $original_dir/$pth" && (cd "$pth" && git pull && go install . && cd "$original_dir") || echo "error"
-done
-
-# pth="catppuccin-bat"
-# BAT_CONFIG_DIR="$(bat --config-dir)"
-# echo "Upgrading $pth" && cd "$pth" && git pull && ...(See README) && cd "$original_dir"
-# bat cache --build
-
-# TODO: The upstream repository was removed or turned private
-#   Expected: https://github.com/ranmaru22/firefox-vertical-tabs
-# May need to find an alternative?
-#
-# pth="firefox-vertical-tabs"
-# echo "Upgrading $pth" && cd "$pth" && git pull && cd "$original_dir"
-#
-# # Note: symlink this project into the Firefox profiles (id's might differ per system)
-# > cd /Users/kyleking/Library/Application\ Support/Firefox/Profiles
-# > lsl
-# Permissions Size User     Date Created Name
-# drwx------@    - kyleking  2 Sep  2022  4c2wgopw.dev-edition-default
-# drwx------@    - kyleking  2 Sep  2022  6zuyes7x.default-release
-# drwx------@    - kyleking  2 Sep  2022  trii2qfj.default
-# > mkcd 4c2wgopw.dev-edition-default/chrome
-# > ln -s /Users/kyleking/Developer/nightly-packages/firefox-vertical-tabs/userChrome.css userChrome.css
-# > ...
-# > # Repeat
+pth="sh/cmd/shfmt"
+echo "Upgrading $original_dir/$pth"
+cd "$pth" && git pull && go install . && cd "$original_dir"
