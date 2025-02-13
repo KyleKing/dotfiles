962e68a34a36bfccd14500a7bce9727dd9027cb0
Kyle King <kyleking@roaminsight.com>
2025-02-13 08:47:04 -0500

    fix: maximize space used by nvim

--- dot_wezterm.lua
+++ dot_wezterm.lua
@@ -206,7 +206,7 @@ config.mouse_bindings = {
 
 -- Brew install fonts and verify installation and name in Apple's "Font Book"
 config.font = wezterm.font_with_fallback({
-    "Atkinson Hyperlegible Mono",
+    -- "Atkinson Hyperlegible Mono",
     "FiraCode Nerd Font Mono",
     "FiraMono Nerd Font Mono",
     "Hack Nerd Font Mono",
@@ -257,5 +257,13 @@ config.window_decorations = "RESIZE"
 config.hide_tab_bar_if_only_one_tab = true
 config.show_tab_index_in_tab_bar = false
 config.show_new_tab_button_in_tab_bar = false
+
+-- Maximize space used by nvim
+config.window_padding = {
+    left = 0,
+    right = 0,
+    top = 0,
+    bottom = 0,
+}
 
 return config
