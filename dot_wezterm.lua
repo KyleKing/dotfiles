0ff58167a1a6370d4276c000e8794ab504e5b059
Kyle King <kyleking@roaminsight.com>
2025-02-24 13:34:51 -0500

    style: increase Wezterm font size

--- dot_wezterm.lua
+++ dot_wezterm.lua
@@ -167,7 +167,7 @@ end)
 
 local config = wezterm.config_builder()
 config.bold_brightens_ansi_colors = true
-config.font_size = 12.5
+config.font_size = 13.5
 config.initial_cols = 200
 config.initial_rows = 60
 config.scrollback_lines = 10000
