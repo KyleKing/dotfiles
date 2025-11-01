d080046773698ef534bf9b9589472c906e8a9bea
Kyle King <KyleKing@users.noreply.github.com>
2025-11-01 06:29:06 -0600

    docs: finish kubectl cleanup

--- dot_config/my_config/private_containers.sh
+++ dot_config/my_config/private_containers.sh
@@ -3,7 +3,7 @@
 # https://github.com/koalaman/shellcheck/issues/809#issuecomment-631194320
 #
 # Configuration for working with containers (Kube, krew, Docker, Podman, etc.)
-#   Note: Kubernetes and Podman snippets removed in: <TBD>
+#   Note: Kubernetes and Podman snippets removed in: <https://github.com/KyleKing/dotfiles/commit/7fe58d4d0a03b6ed51fd89b6b69e0bf15d294d85>
 
 # Source: https://github.com/jesseduffield/lazydocker
 alias lzd='lazydocker'
